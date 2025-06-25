554a4b3b52bae5a17b0f9d2a810f7cb819ad85ca
Luis Andrés Rojas Matey <larmcr@gmail.com>
2025-06-25 17:58:27 -0600

    Cp1 (#4)
    * Caso Práctico 1
    
    * Caso Práctico 1

---
+++ CP1/EF/Context.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace EF
+{
+    private class Context : DbContext
+    {
+        public DbSet<Lotto> Lottos { get; set; }
+        public DbSet<Numero> Numeros { get; set; }
+    }
+
+    public class Lotto
+    { 
+        public int LottoId { get; set; }
+        public DateTime FechaHora { get; set; }
+
+        public virtual List<Numero> Numeros { get; set; }
+}
+
+    public class Numero
+    {
+        public int NumeroId { get; set; }
+        public int Orden { get; set; }
+        public int Num { get; set; }
+
+        public int LottoId { get; set; }
+        public virtual Lotto Lotto { get; set; }
+    }
+}
